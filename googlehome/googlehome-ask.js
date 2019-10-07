624c4021511d27158cdbb5fd27cbfaddffa45166
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-10-07 00:39:49 -0300

    test

--- googlehome/googlehome-ask.js
+++ googlehome/googlehome-ask.js
@@ -27,7 +27,7 @@ module.exports = function(RED) {
                         params: params
                       },
                       gh_messages: [this.message]
-                    }, false);
+                    });
                   });
                   break;
 
