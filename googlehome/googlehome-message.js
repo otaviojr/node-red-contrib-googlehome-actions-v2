1194e409ce95b6d871b43b9ad3e68d9ad697e623
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-10-11 18:10:06 -0300

    message node

--- googlehome/googlehome-message.js
+++ googlehome/googlehome-message.js
@@ -23,10 +23,54 @@ module.exports = function(RED) {
 
         this.name = config.name;
         this.messageType = config.message_type;
+        this.message = config.message;
+        this.permissions = config.permissions;
+        this.url = config.url;
+        this.concatMessage = config.concat_message;
 
         this.on('input', msg => {
             this.debug("GoogleHomeMessageNode - Input Message Received");
             this.log(msg);
+
+            let obj = null;
+            switch(this.messageType){
+                case "SimpleResponse":
+                    obj = {
+                        "type": "SimpleResponse",
+                        "Message": this.message
+                    };
+                    break;
+
+                case "LinkOutSuggestion":
+                    obj = {
+                        "type": "link",
+                        "message": {
+                            "url": this.url
+                        }
+                    };
+                    break;
+
+                case "Permission":
+                    if(this.permissions.length > 0){
+                        obj = {
+                            "type": "permission",
+                            "permission": {
+                                "context": this.message,
+                                "permissions": this.permissions.split(",");
+                            }
+                        };
+                    }
+                    break;
+            }
+
+            if(obj != null){
+                if(this.concatMessage){
+                    msg.gh_messages.push(obj);
+                } else {
+                    msg.gh_messages = [obj];
+                }
+            }
+            this.send(msg);
         });
 
         this.on('close', () => {
